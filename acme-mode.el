5a600f30d513156468b1d75385b5ca77e07ce34e
nothing-c <halskdjf@gmail.com>
2025-04-13 18:54:26 -0500

    Fix an errant quote, add undo & vc to mouse

--- acme-mode.el
+++ acme-mode.el
@@ -9,7 +9,7 @@
 (define-key acme-keymap (kbd "<C-down-mouse-3>") #'(lambda () (interactive) (search-forward (word-at-point))))
 (define-key acme-keymap (kbd "<C-S-mouse-1>") 'insert-parentheses)
 (define-key acme-keymap (kbd "<C-S-mouse-2>") 'shell-command)
-(define-key acme-keymap (kbd "<C-S-mouse-3>") '#'(lambda () (interactive) (search-backward (word-at-point))))
+(define-key acme-keymap (kbd "<C-S-mouse-3>") #'(lambda () (interactive) (search-backward (word-at-point))))
 (define-key acme-keymap (kbd "<M-mouse-1>") 'compile)
 (define-key acme-keymap (kbd "<M-mouse-2>") 'find-file-at-point)
 (define-key acme-keymap (kbd "<M-mouse-3>") 'save-buffer)
@@ -20,6 +20,8 @@
 (define-key acme-keymap (kbd "<mouse-5>") 'next-buffer)
 (define-key acme-keymap (kbd "<C-mouse-4>") 'ibuffer)
 (define-key acme-keymap (kbd "<C-mouse-5>") 'ibuffer)
+(define-key acme-keymap (kbd "<M-mouse-4>") 'undo)
+(define-key acme-keymap (kbd "<M-mouse-5>") 'vc-next-action) ;; No specific redo command
 
 (define-minor-mode acme-mode
   "Minor mode for useful mouse commands, somewhat inspired by Plan9's Acme editor"
